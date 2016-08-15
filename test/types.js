30f9aa350e278f9cd25c545491a7ca843c01a319
Jayson Harshbarger <hypercubed@gmail.com>
2016-08-15 19:19:11 +0900

    Test missing values

--- test/types.js
+++ test/types.js
@@ -14,6 +14,8 @@ test('string', t => {
   t.is(fn(true), 'true');
   t.is(fn(false), 'false');
   t.is(fn(new Date('2019-01-02')), new Date('2019-01-02').toString());
+
+  t.is(fn(''), '');
 });
 
 test('number', t => {
@@ -27,6 +29,8 @@ test('number', t => {
   t.is(fn('INF'), Infinity);
   t.is(fn('-INF'), -Infinity);
 
+  t.is(fn(''), null);
+
   t.throws(() => fn('unknown'));
 });
 
@@ -38,14 +42,40 @@ test('integer', t => {
   t.is(fn('INF'), Infinity);
   t.is(fn('-INF'), -Infinity);
 
+  t.is(fn(''), null);
+
   t.throws(() => fn('1.1'));
   t.throws(() => fn('unknown'));
+  t.throws(() => fn('-'));
+});
+
+test('integer, missing values', t => {
+  const fn = schemaProcessor.generateCastFn({
+    type: 'integer',
+    missingValues: '-'
+  });
+  t.is(fn('1'), 1);
+  t.is(fn('-'), null);
+  t.throws(() => fn(''));
+});
+
+test('integer, two missing values', t => {
+  const fn = schemaProcessor.generateCastFn({
+    type: 'integer',
+    missingValues: ['', '-']
+  });
+  t.is(fn('1'), 1);
+  t.is(fn('-'), null);
+  t.is(fn(''), null);
+  t.throws(() => fn('unk'));
 });
 
 test('datetime', t => {
   const fn = schemaProcessor.generateCastFn({type: 'datetime'});
   t.deepEqual(fn('1990-01-01T00:00:00Z'), utc(1990, 0, 1, 0, 0, 0));
   t.deepEqual(fn('2011-12-31T23:59:59Z'), utc(2011, 11, 31, 23, 59, 59));
+
+  t.is(fn(''), null);
 
   t.throws(() => fn('unknown'));
 });
@@ -55,20 +85,32 @@ test('date', t => {
   t.deepEqual(fn('1990-01-01'), utc(1990, 0, 1));
   t.deepEqual(fn('2011-12-31'), utc(2011, 11, 31));
 
+  t.is(fn(''), null);
+
   t.throws(() => fn('unknown'));
 });
 
 test('date, any', t => {
-  const fn = schemaProcessor.generateCastFn({type: 'date', format: 'any'});
+  const fn = schemaProcessor.generateCastFn({
+    type: 'date',
+    format: 'any'
+  });
   t.deepEqual(fn('2011/12/31 23:59:59Z'), utc(2011, 11, 31, 23, 59, 59));
   t.deepEqual(fn('2011'), new Date('2011'));
+
+  t.is(fn(''), null);
 
   t.throws(() => fn('unknown'));
 });
 
 test('date, fmt', t => {
-  const fn = schemaProcessor.generateCastFn({type: 'date', format: 'fmt:%Y'});
+  const fn = schemaProcessor.generateCastFn({
+    type: 'date',
+    format: 'fmt:%Y'
+  });
   t.deepEqual(fn('2011'), utc(2011, 0, 1, 0, 0, 0));
+
+  t.is(fn(''), null);
 
   t.throws(() => fn('unknown'));
 });
@@ -77,6 +119,8 @@ test('time', t => {
   const fn = schemaProcessor.generateCastFn({type: 'time'});
   t.deepEqual(fn('09:00:00'), utc(1900, 0, 1, 9, 0, 0));
   t.deepEqual(fn('18:00:00'), utc(1900, 0, 1, 18, 0, 0));
+
+  t.is(fn(''), null);
 
   t.throws(() => fn('unknown'));
 });
@@ -87,6 +131,8 @@ test('duration', t => {
   t.is(fn('PT10M'), 10 * 60 * 1000);
   t.is(fn('PT20H'), 20 * 60 * 60 * 1000);
   t.is(fn('PT6M4S'), (6 * 60 * 1000) + 4000);
+
+  t.is(fn(''), null);
 
   t.throws(() => fn('unknown'));
 });
@@ -108,6 +154,8 @@ test('boolean', t => {
   t.is(fn('y'), true);
   t.is(fn('n'), false);
 
+  t.is(fn(''), null);
+
   t.throws(() => fn('unknown'));
 });
 
@@ -116,6 +164,8 @@ test('object', t => {
   t.deepEqual(fn('{}'), {});
   t.deepEqual(fn('{"foo": "bar", "baz": true, "buzz": 10}'), {foo: 'bar', baz: true, buzz: 10});
   t.deepEqual(fn('{foo: "bar", baz: true, "buzz": 10}'), {foo: 'bar', baz: true, buzz: 10});
+
+  t.is(fn(''), null);
 
   t.throws(() => fn('unknown'));
   t.throws(() => fn('{"foo": "bar"'));
@@ -126,6 +176,8 @@ test('array', t => {
   const fn = schemaProcessor.generateCastFn({type: 'array'});
   t.deepEqual(fn('[]'), []);
   t.deepEqual(fn('[1,2,3]'), [1, 2, 3]);
+
+  t.is(fn(''), null);
 
   t.throws(() => fn('unknown'));
   t.throws(() => fn('[1,2,3'));
